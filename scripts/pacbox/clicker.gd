6b74a10ed2647d0308ba2590acc3f5a954c4a43d
Stev51 <dinosaurrampage51@gmail.com>
2026-04-06 13:53:23 -0500

    Clicker display stuff

--- scripts/pacbox/clicker.gd
+++ scripts/pacbox/clicker.gd
@@ -1,10 +1,48 @@
+@tool
 extends Node2D
 
-@export var image: Texture2D
+enum DisplayMode {STILL_IMAGE, ANIMATED_IMAGE, HIDDEN_IMAGE}
 
-@onready var sprite_node = $Sprite2D
+@export var still_image: Texture2D
+@export var animated_image: SpriteFrames
+@export var display_mode: DisplayMode = DisplayMode.STILL_IMAGE
+@export var event_id: int = -1
+
+@onready var still_sprite_node = $Sprite2D
+@onready var anim_sprite_node = $AnimatedSprite2D
 
 func _ready() -> void:
 	
-	if image != null:
-		sprite_node.texture = image
+	still_sprite_node.texture = still_image
+	anim_sprite_node.sprite_frames = animated_image
+	update_display_mode(display_mode)
+
+func update_display_mode(mode: DisplayMode) -> void:
+	
+	display_mode = mode
+	
+	match mode:
+		DisplayMode.STILL_IMAGE:
+			still_sprite_node.visible = true
+			anim_sprite_node.visible = false
+		DisplayMode.ANIMATED_IMAGE:
+			still_sprite_node.visible = false
+			anim_sprite_node.visible = true
+		_:
+			still_sprite_node.visible = false
+			anim_sprite_node.visible = false
+
+func still_img() -> void:
+	update_display_mode(DisplayMode.STILL_IMAGE)
+
+func anim_img() -> void:
+	update_display_mode(DisplayMode.ANIMATED_IMAGE)
+
+func hide_img() -> void:
+	update_display_mode(DisplayMode.HIDDEN_IMAGE)
+
+func enable() -> void:
+	visible = true
+
+func disable() -> void:
+	visible = false
